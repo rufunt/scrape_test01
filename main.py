99a9dcebfa0bfae8c2a6571d1f5d770030f6bf09
rufunt <jazzoo@mail.ru>
2018-03-22 23:16:27 +0300

    Add action for programm nashe-lyubimoe-kino

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ def get_html(url):
 	r = requests.get(url)
 	return r.text
 
+def get_data(html):
+		soup = BeautifulSoup(html, 'lxml')
+		h1 = soup.find('dl').find('dd').find('ul')
+		return h1
+
 def main():
-	url = 'https://wordpress.org/'
-	print(get_html(url))
+	url = 'https://point.md/ru/tv/nashe-lyubimoe-kino'
+	print(get_data(get_html(url)))
 
 
 
