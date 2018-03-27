4ab605e757e1cdfac4b35df1332136d92e564488
rufunt <jazzoo@mail.ru>
2018-03-27 23:42:22 +0300

    Update pattern app

--- 0042/main.py
+++ 0042/main.py
@@ -8,13 +8,19 @@ def get_html(url):
 	r = requests.get(url)
 	if r.ok:
 		return r.text
-	print(r.status_code)	
+	print(r.status_code)
+
+
+def write_csv(data):
+	with open(nnm.csv, 'a') as f:
+	    writer = csv.writer(f)
+	    pass		
 
 
 
 
 def main():
-	url = 'https://yandex.ru/yaca/cat/Entertainment/'
+	url = 'http://itog.info/blogs/page1/'
 	print(get_html(url))
 
 
