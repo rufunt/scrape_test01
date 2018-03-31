7d42880cd28d8e2f7d21ad214dafa1ab10889c0d
rufunt <jazzoo@mail.ru>
2018-03-31 21:23:04 +0300

    Update app

--- 005/main.py
+++ 005/main.py
@@ -6,7 +6,9 @@ def write_csv(data):
 
 
 def main():
-	pass
+	d = {'name': 'Petr', 'surname': 'Ivanov', 'age': 33}
+	n = {'name': 'Fedr', 'surname': 'Petrov', 'age': 23}
+	c = {'name': 'Petro', 'surname': 'Sidorov', 'age': 53}
 
 
 if __name__ == '__main__':
