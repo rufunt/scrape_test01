bb29060767441488b1f9e349e14466f46c7451ac
rufunt <jazzoo@mail.ru>
2018-04-03 20:34:25 +0300

    Add loop for parsing all pages

--- 008/liveinternet.py
+++ 008/liveinternet.py
@@ -15,21 +15,23 @@ def write_csv(data):
 
 
 def main():
-    url = 'https://www.liveinternet.ru/rating/ru//today.tsv?page=2'
-    response = get_html(url)
-    data = response.strip().split('\n')[1:]
 
-    for row in data:
-        columns = row.strip().split('\t')
-        name = columns[0]
-        url = columns[1]
-        description = columns[2]
-        traffic = columns[3]
-        percent = columns[4]
+    for i in range(0, 10):
+        url = 'https://www.liveinternet.ru/rating/ru//today.tsv?page={}'.format(str(i))
+        response = get_html(url)
+        data = response.strip().split('\n')[1:]
 
-        data = {'name': name, 'url': url, 'description': description, 'traffic': traffic, 'percent': percent}
+        for row in data:
+            columns = row.strip().split('\t')
+            name = columns[0]
+            url = columns[1]
+            description = columns[2]
+            traffic = columns[3]
+            percent = columns[4]
 
-        write_csv(data)
+            data = {'name': name, 'url': url, 'description': description, 'traffic': traffic, 'percent': percent}
+
+            write_csv(data)
 
 if __name__ == '__main__':
     main()
